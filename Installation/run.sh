5b51467709adf50c7212d269712790ab4144aafe
Mark Johnson <markfjohnson@gmail.com>
2018-08-21 15:23:42 -0400

    Changed requests to urllib3

--- Installation/run.sh
+++ Installation/run.sh
@@ -1,1 +1,1 @@
-dcos spark run --submit-args="--isPython --conf spark.mesos.executor.docker.forcePullImage=True --conf spark.mesos.executor.docker.image=markfjohnson/cassandra_spark https://raw.githubusercontent.com/markfjohnson/sec-access/master/src/indexes/xbrl_rss_reader.py -f 2018 -t 2019" --verbose
+dcos spark run --submit-args="--conf spark.mesos.executor.docker.image=markfjohnson/cassandra_spark https://raw.githubusercontent.com/markfjohnson/sec-access/master/src/indexes/xbrl_rss_reader.py -f 2018 -t 2019" --verbose
