ced730320f6bf73f30ad3aeccbbb8193dbb77d0a
Mark Johnson <markfjohnson@gmail.com>
2018-08-21 12:17:50 -0400

    Changed the dockerfile

---
+++ Installation/run.sh
@@ -0,0 +1,1 @@
+dcos spark run --submit-args="--conf spark.mesos.executor.docker.image=markfjohnson/cassandra_spark https://raw.githubusercontent.com/markfjohnson/sec-access/master/src/indexes/xbrl_rss_reader.py -f 2018 -t 2019" --verbose
