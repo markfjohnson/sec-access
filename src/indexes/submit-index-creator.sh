6fa10a5dbdbd50792318fd3d10bd8d149a60e322
Mark Johnson <markfjohnson@gmail.com>
2018-09-18 19:15:41 -0700

    added save to csv

--- src/indexes/submit-index-creator.sh
+++ src/indexes/submit-index-creator.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dcos spark run --submit-args="--conf spark.mesos.executor.docker.image=markfjohnson/cassandra_spark https://raw.githubusercontent.com/markfjohnson/sec-access/master/src/indexes/xbrl_rss_reader.py -f 2018 -t 2019" --verbose --name=spark
+dcos spark run --submit-args="--conf spark.mesos.executor.docker.image=markfjohnson/cassandra_spark https://raw.githubusercontent.com/markfjohnson/sec-access/master/src/indexes/xbrl_rss_reader.py -f 2018 -t 2018" --verbose --name=spark
